82e417fd5505f31567f15ebbdadb127f8d655cd2
Haniya <haniyasajjad18@gmail.com>
2025-05-15 11:00:29 +0500

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'project_tracker',
-    password: '123456', // Match the password you set, or remove if none
+    password: 'your_password', // Match the password you set, or remove if none
     port: 5432,
 });
 
